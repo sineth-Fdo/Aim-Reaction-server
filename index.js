334048f5517b4c66245bcb1523f0febc67977699
sineth-Fdo <132679708+sineth-Fdo@users.noreply.github.com>
2024-05-22 22:16:10 +0530

    feat: add won message

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const io = new Server(server, {
 });
 
 
+
 io.of("/home").on('connection', (socket) => {
     console.log(`User Connected : ${socket.id}`);
 
@@ -46,13 +47,31 @@ io.of("/home").on('connection', (socket) => {
         console.log(`Room ${room} has been deleted`);
     })
 
-
+    // Get room count
+    socket.on("get-room-count", (room) => {
+        const roomCount = io.of("/home").adapter.rooms.get(room)?.size || 0;
+        console.log(`Room ${room} has ${roomCount} users`);
+        io.of("/home").to(room).emit("room-count", roomCount); 
+    });
 
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
     })
+
+    //send win message
+    socket.on("win_message", (data, room) => {
+        socket.to(room).emit("receive_win", data);
+        console.log(`${data.id} Won from room ${room}`);
+    });
     
 });
+
+//get room count
+app.get("/room-count", (req, res) => {
+
+    res.json(roomCount);
+});
+
 
 
 
